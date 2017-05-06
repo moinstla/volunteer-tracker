4ad6ef11487850eabb163a7d29e5d2f0f401265f
Monique St. Laurent <moinstla@gmail.com>
2017-05-05 17:07:23 -0700

    add routing for posting projects and volunteers

--- app.rb
+++ app.rb
@@ -9,5 +9,24 @@ require 'pry'
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get '/' do
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:index)
+end
+
+post '/project' do
+  project = params.fetch("project")
+  new_project = Project.new(:name => project, :id => nil)
+  new_project.save
+  @projects = Project.all
+  erb(:index)
+end
+
+post '/volunteer' do
+  volunteer = params.fetch('volunteer')
+  project_id = params.fetch('project_id').to_i
+  new_volunteer = Volunteer.new(:name => new_volunteer, :project_id => project_id, :id => nil)
+  new_volunteer.save
+  @volunteers = Volunteer.all
   erb(:index)
 end
